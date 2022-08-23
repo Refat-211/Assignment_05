c8d11b9f6ba45a0880d9430f6b3154221445b72c
unknown <refathassan512@gmail.com>
2022-08-23 16:23:12 +0600

    player list added

--- js/psg.js
+++ js/psg.js
@@ -1,21 +1,76 @@
 
 
-function displayPlayerNames (playerList){
-    // const playerListSerial = document.getElementById('player-list');
+// function displayPlayerNames (){
 
-}
+//     if (playerNamesArray.length <= 5){
+
+//         document.getElementById("selected-name").innerText = playerNamesArray.length;
+
+//         const listContainer = document.getElementById("player-list");
+//         listContainer.textContent = '';
+
+//         for (let i = 0; i < playerNamesArray.length; i++) {
+
+//         const tr = document.createElement("tr");
+//         tr.innerHTML = `
+//         <th>${i + 1}</th>
+//         <td>${playerNamesArray[i].playerName}</td>
+//         `;
+//             listContainer.appendChild(tr);
+//         }
+//     }
+   
+//     else{
+//     alert('You can be able to select maximum five players.');
+// }
+
+// }
+
+
+// const playerNamesArray = [];
+
+// function addToList(element) {
+//     element.disabled = true;
+
+//     const playerName = element.parentNode.parentNode.children[0].innerText;
+
+//     playerNamesArray.push(playerName);
+
+//     displayPlayerNames();
+// }
+
 
 
 const playerNamesArray = [];
 
 function addToList(element) {
-    element.disabled = true;
 
+    element.disabled = true;
+    
     const playerName = element.parentNode.parentNode.children[0].innerText;
 
     playerNamesArray.push(playerName);
+    if (playerNamesArray.length <= 5) {
 
-    document.getElementById("selected-name").innerText = playerNamesArray.length;
+        document.getElementById("selected-name").innerText = playerNamesArray.length;
 
-    displayPlayerNames(playerNamesArray);
+        const listContainer = document.getElementById("player-list");
+        listContainer.textContent = '';
+
+        for (let i = 0; i < playerNamesArray.length; i++) {
+
+            const tr = document.createElement("tr");
+            tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${playerNamesArray[i].playerName}</td>
+        `;
+            listContainer.appendChild(tr);
+        }
+    }
+
+    else {
+        alert('You can be able to select maximum five players.');
+        element.disabled = false;
+    }
+    
 }
