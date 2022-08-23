69a69299713ad50d9c348cc90e9039186dccf229
unknown <refathassan512@gmail.com>
2022-08-23 18:16:17 +0600

    selected part some issue fixed

--- js/psg.js
+++ js/psg.js
@@ -1,46 +1,3 @@
-
-
-// function displayPlayerNames (){
-
-//     if (playerNamesArray.length <= 5){
-
-//         document.getElementById("selected-name").innerText = playerNamesArray.length;
-
-//         const listContainer = document.getElementById("player-list");
-//         listContainer.textContent = '';
-
-//         for (let i = 0; i < playerNamesArray.length; i++) {
-
-//         const tr = document.createElement("tr");
-//         tr.innerHTML = `
-//         <th>${i + 1}</th>
-//         <td>${playerNamesArray[i].playerName}</td>
-//         `;
-//             listContainer.appendChild(tr);
-//         }
-//     }
-   
-//     else{
-//     alert('You can be able to select maximum five players.');
-// }
-
-// }
-
-
-// const playerNamesArray = [];
-
-// function addToList(element) {
-//     element.disabled = true;
-
-//     const playerName = element.parentNode.parentNode.children[0].innerText;
-
-//     playerNamesArray.push(playerName);
-
-//     displayPlayerNames();
-// }
-
-
-
 const playerNamesArray = [];
 
 function addToList(element) {
@@ -50,20 +7,20 @@ function addToList(element) {
     const playerName = element.parentNode.parentNode.children[0].innerText;
 
     playerNamesArray.push(playerName);
+
     if (playerNamesArray.length <= 5) {
 
         document.getElementById("selected-name").innerText = playerNamesArray.length;
 
         const listContainer = document.getElementById("player-list");
-        listContainer.textContent = '';
+        listContainer.innerHTML = "";
 
         for (let i = 0; i < playerNamesArray.length; i++) {
 
+            const playerNameList = playerNamesArray[i];
             const tr = document.createElement("tr");
-            tr.innerHTML = `
-        <th>${i + 1}</th>
-        <td>${playerNamesArray[i].playerName}</td>
-        `;
+            tr.innerHTML = `<th>${i + 1}</th>
+            <td>${playerNameList}</td>`;
             listContainer.appendChild(tr);
         }
     }
